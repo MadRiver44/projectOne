67cca780c3bcbc712ec586476987a384b4cbf200
Kevin Turney <super996@optonline.net>
2016-12-14 17:48:25 -0500

    all divs clickable

--- src/app.js
+++ src/app.js
@@ -1,2 +1,17 @@
+/* eslint-env jquery */
 
+const pageLoad = $('document').ready(function () {
+  for (let i = 0; i < $('.parent').children().length; i += 1) {
+    $('.square').click(function (e) {
+      $(this).appendTo('.empty');
+    });
+  }
+});
+
+// * get  inner_square to move to empty div when clicked
+$('.inner_square3').click(function () {
+  $('.inner_square3').appendTo('.empty');
+});
+
+// when page loads all parent divs are clickable to .empty class div
 
