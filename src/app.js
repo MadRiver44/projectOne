d68f7ca416f1ab8a1ebdbd75949f5d4ea59c54d4
Kevin Turney <super996@optonline.net>
2016-12-15 15:30:21 -0500

    check win function

--- src/app.js
+++ src/app.js
@@ -1,17 +1,49 @@
 /* eslint-env jquery */
+/*const classSquares = document.querySelectorAll('.square'); // children
+let elementIdOrder = [];
+for (let i = 0; i < classSquares.length; i += 1) { // loop over class square
+  //console.log(classSquares[i]); // all items are in div id order
+  let x = classSquares[i].innerHTML
+  elementIdOrder.push(parseInt(x));
+}*/
 
-const pageLoad = $('document').ready(function () {
-  for (let i = 0; i < $('.parent').children().length; i += 1) {
-    $('.square').click(function (e) {
-      $(this).appendTo('.empty');
-    });
-  }
+/* stackoverflow question answered */
+$('document').ready(function () {
+  $('.square').click(function () {
+    const parent = $(this).parent('div');
+    console.log(this);
+    $(this).appendTo('.empty');
+    $('.empty').addClass('full').removeClass('empty');
+    parent.addClass('empty').removeClass('full');
+  });
 });
 
-// * get  inner_square to move to empty div when clicked
-$('.inner_square3').click(function () {
-  $('.inner_square3').appendTo('.empty');
-});
+/* win state - loop over array of ids and if they are mapped
+in a particular order, console.log('WIN'); */
 
-// when page loads all parent divs are clickable to .empty class div
+let winningArrayPattern = [0, 1, 2]; //parent id's in order, immutable
+let currentArrayPattern = [44, 2, 3] // i will look for the child element with parent id of the index of the winning Arr
+                                //  i will then set that index value in currentArray to that child value,
+                                // when the arrays match, win!!
+
+const classSquares = document.querySelectorAll('.square'); // children in a NodeList
+//console.log(classSquares.length) // 3
+//console.log(classSquares[1].innerHTML) // finally !!  child's innerHTML - parse it to a number
+
+
+// i have the values, now to map them to parent indexes
+for (let i = 0; i < classSquares.length; i += 1){
+  // the parent of each child
+ //console.log(classSquares[i].innerHTML)
+//console.log(classSquares[i].parentNode);
+
+// got parent id !!!!
+let parentID = classSquares[i].parentNode;
+console.log(parentID.id);
+
+}
+
+
+
+
 
